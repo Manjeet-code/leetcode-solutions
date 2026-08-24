b5e951cecf5b91feb6951f713ffb65b158a8db86
Manjeet Kumar <2024021139@mmmut.ac.in>
2026-08-24 23:36:35 +0530

    Time: 0 ms (100%), Space: 13.4 MB (45.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,50 +1,25 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-
+        int m = matrix.size(); // no. of rows
+        int n = matrix[0].size(); // no. of cols
         int low = 0;
-        int high = n - 1;
-        int row = -1;
+        int high = m*n - 1;
 
-        // Find the possible row
         while(low <= high) {
             int guess = (low + high) / 2;
-
-            if(matrix[guess][0] <= target) {
-                row = guess;
-                low = guess + 1;
+            int row = guess/n;
+            int col = guess%n;
+            if(matrix[row][col] == target) {
+               return true;
+            }
+            if(matrix[row][col] < target){
+                low = guess+1;
             }
             else {
                 high = guess - 1;
             }
         }
-
-        // Target is smaller than first element
-        if(row == -1) {
-            return false;
-        }
-
-        int m = matrix[0].size();
-
-        low = 0;
-        high = m - 1;
-
-        // Binary search in selected row
-        while(low <= high) {
-            int guess = (low + high) / 2;
-
-            if(matrix[row][guess] == target) {
-                return true;
-            }
-            else if(matrix[row][guess] < target) {
-                low = guess + 1;
-            }
-            else {
-                high = guess - 1;
-            }
-        }
-
         return false;
     }
 };
