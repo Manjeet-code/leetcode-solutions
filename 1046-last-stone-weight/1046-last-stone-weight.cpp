787519e165df40cbda85278d8af79174477865e7
Manjeet Kumar <2024021139@mmmut.ac.in>
2026-09-01 00:51:57 +0530

    Time: 0 ms (100%), Space: 10 MB (12.56%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();
+        priority_queue<int>pq;
+        for(int i=0;i<n;i++){
+            pq.push(stones[i]);
+        }
+        while(pq.size()>1){
+        int a = pq.top();
+        pq.pop();
+        int b = pq.top();
+        pq.pop();
+        if(a!=b){
+            int c = abs(a-b);
+            pq.push(c);
+        } 
+        }
+        if(pq.empty())
+        return 0;
+        return pq.top(); 
+    }
+};
