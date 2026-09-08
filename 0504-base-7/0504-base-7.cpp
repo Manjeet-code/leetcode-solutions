e9a1838bb0886be8e0d35dd8c6734904bb56d211
Manjeet Kumar <2024021139@mmmut.ac.in>
2026-09-08 21:40:35 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.52%) - LeetHub

---
+++ 0504-base-7/0504-base-7.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if(num == 0)
+            return "0";
+
+        bool negative = num < 0;
+        num = abs(num);
+
+        string ans = "";
+
+        while(num > 0) {
+            int rem = num % 7;
+            ans += char('0' + rem);
+            num = num / 7;
+        }
+
+        if(negative)
+            ans += '-';
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
